4ab3d82d81a37e4051c5a7f3af32ee1286e1e31a
Aswin Reji <aswinreji.004@gmail.com>
2024-08-20 13:49:54 +0000

    react useEffect basics learning

---
+++ src/useEffect.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [resourceType, setResourseType] = useState("users");
+  const [count,setCount] = useState(0)
+    useEffect(()=>{
+        console.log('render ',count);
+        // If we put empty array it means it is when initialized then this effect will work
+        // onMount
+    },[])
+  return (
+    <>
+      <div>
+        <button onClick={() => setResourseType("users")}>users</button>
+        <button onClick={() => setResourseType("posts")}>posts</button>
+        <button onClick={() => setResourseType("comments")}>comments</button>
+        <button onClick={()=> setCount(prevCount=>prevCount + 1)}>Increment</button>
+      </div>
+      <h1>{resourceType}</h1>
+      <h1>{count}</h1>
+    </>
+  );
+}
+
+export default App;
