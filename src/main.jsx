80f284f15b54a77b5676694fe6d98fa03e5241a2
Aswin Reji <aswinreji.004@gmail.com>
2024-08-20 14:46:48 +0000

    window resizing using useState and useEffect and adding removing event

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import UseEffectLearning from "./useEffect";
+import { WindowWidthChecker } from "./windowWidth";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UseEffectLearning />{" "}
+    <WindowWidthChecker />
   </StrictMode>
 );
--- src/useEffect.jsx
+++ src/useEffect.jsx
@@ -2,19 +2,24 @@ import { useEffect, useState } from "react";
 
 function UseEffectLearning() {
   const [resourceType, setResourseType] = useState("users");
-  const [count,setCount] = useState(0)
-    useEffect(()=>{
-        console.log('render ',count);
-        // If we put empty array it means it is when initialized then this effect will work
-        // onMount
-    },[])
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("render ", count);
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    // If we put empty array it means it is when initialized then this effect will work
+    // onMount
+  }, []);
   return (
     <>
       <div>
         <button onClick={() => setResourseType("users")}>users</button>
         <button onClick={() => setResourseType("posts")}>posts</button>
         <button onClick={() => setResourseType("comments")}>comments</button>
-        <button onClick={()=> setCount(prevCount=>prevCount + 1)}>Increment</button>
+        <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+          Increment
+        </button>
       </div>
       <h1>{resourceType}</h1>
       <h1>{count}</h1>
