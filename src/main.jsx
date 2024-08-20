779ad4bd089672959452b357b7d3ea1c5b4eb12d
Aswin Reji <aswinreji.004@gmail.com>
2024-08-20 15:08:04 +0000

    basics react props

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import UseEffectLearning from "./useEffect";
 import { WindowWidthChecker } from "./windowWidth";
+import { LearningProps } from "./props";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UseEffectLearning />
+    {/* <UseEffectLearning /> */}
+    {/* <WindowWidthChecker /> */}
+    < LearningProps name="panda" />
   </StrictMode>
 );
---
+++ src/props.jsx
@@ -0,0 +1,4 @@
+export function LearningProps({ name }) {
+  console.log(name, "props is showing");
+  return <h1>Hello World , {name}</h1>;
+}
